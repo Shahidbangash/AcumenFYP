973d6ba1ecdd5e5ccffedc6f5e3d0310acc8c1e1
ShahidBangash <shahidbangashsi94@gmail.com>
2021-05-29 08:44:10 +0500

    issue with css fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-const video = document.getElementById("video");
-
+var video;
 $(document).ready(function () {
   $(".main-header").load("./header.html");
 });
@@ -38,23 +37,52 @@ function displayYouTubeVideo() {
 }
 
 function getResult() {
+  $(".pre-loader").append(
+    "<div><h2>We are loading Modal </h2><div class='loader ' id='loader'></div></div> "
+  );
+
   Promise.all([
     // To load all the models
     faceapi.nets.tinyFaceDetector.loadFromUri(
       "/face-expression-detector/models"
-    ), //remove "/face-expression-detector" when working locally
+    ),
     faceapi.nets.faceLandmark68Net.loadFromUri(
       "/face-expression-detector/models"
-    ), //remove "/face-expression-detector" when working locally
+    ),
     faceapi.nets.faceRecognitionNet.loadFromUri(
       "/face-expression-detector/models"
-    ), //remove "/face-expression-detector" when working locally
+    ),
     faceapi.nets.faceExpressionNet.loadFromUri(
       "/face-expression-detector/models"
-    ), //remove "/face-expression-detector" when working locally
+    ),
   ]).then(startVideo);
 }
 function startVideo() {
+  $("#video-result").empty();
+  $("#video-result").append(`<video id="video" autoplay muted sr></video>`);
+  video = document.getElementById("video");
+
+  video.addEventListener("play", () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    // document.body.append(canvas);
+    document.getElementById("video-result").appendChild(canvas);
+    const displaySize = { width: 720, height: 520 };
+    faceapi.matchDimensions(canvas, displaySize);
+
+    document.querySelector(".pre-loader").innerHTML = "";
+
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    }, 100);
+  });
   // document.querySelector("#loader").classList.add("hide-loader");
   navigator.getUserMedia(
     { video: {} },
@@ -62,22 +90,3 @@ function startVideo() {
     (err) => console.error(err)
   );
 }
-
-video.addEventListener("play", () => {
-  const canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
-  document.getElementById("video-result").appendChild(canvas);
-  const displaySize = { width: 720, height: 520 };
-  faceapi.matchDimensions(canvas, displaySize);
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    faceapi.draw.drawDetections(canvas, resizedDetections);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-  }, 100);
-});
