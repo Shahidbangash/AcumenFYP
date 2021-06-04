332ce1991b0fc649e086b753249eaa22927857d0
ShahidBangash <shahidbangashsi94@gmail.com>
2021-06-05 01:41:14 +0500

    added Sign up and login using firebase

--- js/authentication.js
+++ js/authentication.js
@@ -1,19 +1,18 @@
 function login() {
-    // var { email }
-    // const form_data = document.getElementById('login-form');
-  
-    const form_object = new FormData(document.forms.login_form);
-    // console.log(form_object.get("email"));
-    // console.log(document.getElementById('login-form').val)
+    const formObject = new FormData(document.forms.login_form);
     firebase
       .auth()
       .signInWithEmailAndPassword(
-        form_object.get("email"),
-        form_object.get("password")
+        formObject.get("email"),
+        formObject.get("password")
       )
       .then((userCredential) => {
         // Signed in
         // ...
+        document.getElementById(
+            "login_response"
+          ).innerHTML = `Login is successful`;
+        
       })
       .catch((error) => {
         var errorCode = error.code;
@@ -38,14 +37,18 @@ function login() {
         // console.log(user);
         const userId = userCredential.user.uid;
         const user = {
-          firstName: form_object.get("first_name"),
-          lastName: form_object.get("last_name"),
+          displayName: form_object.get("first_name"),
+        //   lastName: form_object.get("last_name"),
           email: form_object.get("email"),
         };
-        const db = firebase.firestore();
-        db.collection("users").doc(userId).set(userData);
+        // const db = firebase.firestore();
+        // db.collection("users").doc(userId).set(userData);
   
         console.log("We have inserted data of ", user);
+
+        document.getElementById(
+            "login_response"
+          ).innerHTML = `Signup is successful`;
   
         // ...
       })
@@ -70,5 +73,27 @@ function login() {
         console.log("Cannot login ", error);
         // An error happened.
       });
+  }
+
+  function showOutput() {
+    
+    let form = document.getElementsByClassName("custom-form")[0];
+    let form_data = new FormData(form);
+    console.log(form_data.get("no-of-days"));
+  
+    var product_names = document.getElementsByClassName("product-name");
+    //   var trial_days = document.getElementsByClassName("trial-days");
+  
+    for (var i = 0; i < product_names.length; i++) {
+      product_names[i].innerHTML = form_data.get("product-name");
+    }
+    document.getElementById("form-output").style.display = "block";
+  }
+  
+  function switchForm(show_form, hide_form) {
+    document.getElementById("login_form").reset();
+    document.getElementById("signup_form").reset();
+    document.getElementById(hide_form).classList.add("d-none");
+    document.getElementById(show_form).classList.remove("d-none");
   }
   
